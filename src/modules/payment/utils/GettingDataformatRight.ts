77a221545970da2f1d5c50feb690a9b0edc90c36
rodrigo <javierluis15975@hotmail.com>
2025-02-01 18:50:30 -0500

    update node.js.yml, clients.service.ts and config-rabbitMQ.ts

--- src/modules/payment/utils/GettingDataformatRight.ts
+++ src/modules/payment/utils/GettingDataformatRight.ts
@@ -14,6 +14,7 @@ export const gettingDataformatRight = (data: CreatePaymentDto[]) => {
         productVariant,
         size,
         id,
+        description,
       }) => ({
         product,
         brand,
@@ -25,6 +26,7 @@ export const gettingDataformatRight = (data: CreatePaymentDto[]) => {
         productVariant,
         size,
         id,
+        description,
       }),
     )
     return dataCorrect
